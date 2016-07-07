3065529f6d0ba794fc06504c7a0a069f8511c523
Aitor <riturrioz@gmail.com>
2016-07-07 12:52:26 +0200

    install.sh file improved and habmin.zip file renamed

--- install.sh
+++ install.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+## Global variables
+addons_dir="/usr/share/openhab/addons"
+habmin_dir="/usr/share/openhab/webapps/habmin"
+webapps_dir="/usr/share/openhab/webapps"
+slash="/"
+
 ## Make sure only root can run our script
 if [[ $EUID -ne 0 ]]; then
    echo -e "This script must be run as root" 1>&2
    exit 1
 fi
+
+## Check if addons and habmin folders exist
+if [ ! -d "$addons_dir" ]; then
+    echo -e "The $addons_dir does not exist"
+    exit 1
+fi
+if [ ! -d "$habmin_dir" ]; then
+    echo -e "The $habmin_dir does not exist"
+    exit 1
+fi
+
 
 
 ## Ask for the number of Z-Wave bindings to install and check if the answer is valid
@@ -24,12 +41,6 @@ else
 fi
 
 
-## Global variables
-addons_dir="/usr/share/openhab/addons"
-habmin_dir="/usr/share/openhab/webapps/habmin"
-slash="/"
-
-
 ## Installing the required programs
 echo -e '\nInstalling the required programs...'
 apt-get --assume-yes install git unzip
@@ -37,10 +48,17 @@ apt-get --assume-yes install git unzip
 
 ## Cloning the github repository
 cd /tmp
+repository_name="openhab-distributed-zwaves"
+if [ -d "$repository_name" ]; then
+	echo -e "\nThe github repository already exists, let's make a 'git pull'..."
+	cd $repository_name
+	git pull
+else
+	echo -e "\nCloning the github repository..."
+	git clone https://github.com/bodiroga/openhab-distributed-zwaves.git
+	cd $repository_name
+fi
 current_folder=$(pwd)
-echo -e '\nCloning the github repository...'
-git clone https://github.com/bodiroga/openhab-distributed-zwaves.git
-cd openhab-distributed-zwaves
 
 
 ## Get the new HABmin file path
@@ -64,12 +82,13 @@ echo $old_habmin_file
 
 
 ## Get the new Z-Wave bindings files paths
+zwave_word="zwave"
 zwaves_to_install=()
 for (( i=2; i<=$n_bindings; i++ ))
 do
 	zwave_search="$search_start$i$search_end"
-        zwave_file=$(find zwave -name "$zwave_search")
-        zwaves_to_install+=($current_folder$slash$zwave_file)
+        zwave_file=$(find $current_folder$slash$zwave_word -name "$zwave_search")
+        zwaves_to_install+=($zwave_file)
 done
 
 echo -e "\n\nZWaves to install:"
@@ -80,9 +99,7 @@ zwaves_to_delete=()
 for (( i=2; i<=$n_bindings; i++ ))
 do
 	zwave_search="$search_start$i$search_end"
-        echo $zwave_search
         zwave_file=$(find $addons_dir -name "$zwave_search")
-        echo $zwave_file
         zwaves_to_delete+=($zwave_file)
 done
 
@@ -93,16 +110,26 @@ echo ${zwaves_to_delete[@]}
 
 
 echo -e 'Deleting the old HABmin addon...'
+#rm $old_habmin_file
 
 echo -e 'Adding the new HABmin addon...'
+#cp $new_habmin_file $addons_dir
 
 echo -e 'Deleting the old Z-Wave bindings...'
+
 
 echo -e 'Adding the new Z-Wave bindings...'
 
 
-echo -e "Unziping the HABmin folder..."
-
+echo -e "Unziping and copying the HABmin folder..."
+echo $(pwd)
+cd habmin
+echo $(pwd)
+zip_file="habmin (unzip in the webapps folder).zip"
+unzip $zip_file
+cp habmin $webapps_dir
+rm -r habmin
+cd ..
 
 echo -e '\nRemoving the tmp folder...'
-rm -rf /tmp/openhab-distributed-zwaves
+#rm -rf /tmp/openhab-distributed-zwaves
