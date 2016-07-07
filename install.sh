a228a553ca1a7973acf07573df4aba692ff2aec5
Aitor <riturrioz@gmail.com>
2016-07-07 18:04:08 +0200

    Give the users the option to choose if they want to delete or keep the git repository

--- install.sh
+++ install.sh
@@ -9,14 +9,15 @@ slash="/"
 min_n_bindings=2
 max_n_bindings=8
 
-## Make sure only root can run our script
+
+## Making sure only root can run our script
 if [[ $EUID -ne 0 ]]; then
    echo -e "This script must be run as root" 1>&2
    exit 1
 fi
 
 
-## Check if addons and habmin folders exist
+## Checking if addons and habmin folders exist
 if [ ! -d "$addons_dir" ]; then
     echo -e "The $addons_dir does not exist"
     exit 1
@@ -27,7 +28,7 @@ if [ ! -d "$habmin_dir" ]; then
 fi
 
 
-## Ask for the number of Z-Wave bindings to install and check if the answer is valid
+## Asking for the number of Z-Wave bindings to install and check if the answer is valid
 echo -e "How many Z-wave bindings are you going to use? "
 read n_bindings;
 re='^[0-9]+$'
@@ -67,7 +68,7 @@ fi
 current_folder=$(pwd)
 
 
-## Get the new HABmin file path
+## Getting the new HABmin file path
 search_start='*zwave'
 search_end='*'
 habmin_word="habmin"
@@ -79,7 +80,7 @@ echo -e "\n\nHABmin to install:"
 echo $new_habmin_file
 
 
-## Get the old HABmin file path
+## Getting the old HABmin file path
 habmin_search="*habmin*"
 old_habmin_file=$(find $addons_dir -name "$habmin_search")
 
@@ -87,7 +88,7 @@ echo -e "\n\nHABmin to delete:"
 echo $old_habmin_file
 
 
-## Get the new Z-Wave bindings files paths
+## Getting the new Z-Wave bindings files paths
 zwave_word="zwave"
 zwaves_to_install=()
 for (( i=2; i<=$n_bindings; i++ ))
@@ -100,7 +101,9 @@ done
 echo -e "\n\nZWaves to install:"
 echo ${zwaves_to_install[@]}
 
-## Get the old Z-Wave bindings files paths
+
+## Getting the old Z-Wave bindings files paths
+search_start='*.zwave'
 zwaves_to_delete=()
 for (( i=$min_n_bindings; i<=$max_n_bindings; i++ ))
 do
@@ -113,41 +116,57 @@ echo -e "\n\nZWaves to delete:"
 echo ${zwaves_to_delete[@]}
 
 
-
-
+## Deleting the old HABmin addon
 echo -e 'Deleting the old HABmin addon...'
 rm $old_habmin_file
 
+
+## Deleting the old Z-Wave bindings
 echo -e 'Deleting the old Z-Wave bindings...'
 for i in "${zwaves_to_delete[@]}"
 do
 	rm $i
 done
 
+
+## Adding the new HABmin addon
 echo -e 'Adding the new HABmin addon...'
 cp $new_habmin_file $addons_dir
 
+
+## Adding the new Z-Wave bindings
 echo -e 'Adding the new Z-Wave bindings...'
 for i in "${zwaves_to_install[@]}"
 do
 	cp $i $addons_dir
 done
 
+
+## Unziping and copying the HABmin folder
 echo -e "Unziping and copying the HABmin folder..."
 cd habmin
 zip_file="habmin.zip"
-unzip $zip_file >/dev/null
+unzip $zip_file > /dev/null
 cp -r habmin $webapps_dir
 rm -r habmin
 cd ..
 
-echo -e '\nRemoving the tmp folder...'
-#rm -rf /tmp/openhab-distributed-zwaves
 
+## Removing the git repository
+read -p "Do you want to remove the git repository from your computer (y/N)? " choice
+case "$choice" in 
+  y|Y ) echo -e "Deleting the git repository..."; rm -rf /tmp/openhab-distributed-zwavesd;;
+  * ) echo -e "Keeping the git repository...";;
+esac
+
+
+## Restarting openHAB
 echo -e "\nRestarting openHAB..."
 read -p "Do you want to delete old openHAB log files (y/N)? " choice
 case "$choice" in 
   y|Y ) echo -e "Deleting old log files..."; rm $logs_dir$slash$search_end;;
-  * ) echo -e "Keeping old log files";;
+  * ) echo -e "Keeping old log files...";;
 esac
 /etc/init.d/openhab restart > /dev/null
+
+echo -e "\Done."
